92ddbb17526ed3b4e50d620929cf5787066ef714
myvlink <themyvlink@gmail.com>
2018-07-24 18:51:07 +0500

    переделка цикла под итератор

--- js/big-picture.js
+++ js/big-picture.js
@@ -31,36 +31,36 @@
       document.querySelector('.likes-count').textContent = pictures[index].likes;
       document.querySelector('.comments-count').textContent = pictures[index].comments.length;
       var fragment = document.createDocumentFragment();
-      for (var i = 0; i < pictures[index].comments.length; i++) {
-        var comment = document.createElement('li');
+      pictures[index].comments.forEach(function (comment) {
+        var commentElement = document.createElement('li');
         var avatar = document.createElement('img');
         var text = document.createElement('p');
-        comment.classList.add('social__comment', 'social__comment--text');
+        commentElement.classList.add('social__comment', 'social__comment--text');
         avatar.classList.add('social__picture');
         avatar.src = 'img/avatar-' + window.utils.generateInteger(1, 6) + '.svg';
         avatar.alt = 'Аватар комментатора фотографии';
         avatar.width = '35';
         avatar.height = '35';
         text.classList.add('social__text');
-        text.textContent = pictures[index].comments[i];
-        comment.appendChild(avatar);
-        comment.appendChild(text);
-        fragment.appendChild(comment);
+        text.textContent = comment;
+        commentElement.appendChild(avatar);
+        commentElement.appendChild(text);
+        fragment.appendChild(commentElement);
         document.querySelector('.social__caption').textContent = window.data.generateDescription();
-      }
+      });
       document.querySelector('.social__comments').appendChild(fragment);
     },
     bigPictureAssemblyData: function (data) {
       var userImages = document.querySelectorAll('.picture__img');
       gallery.addEventListener('click', function (evt) {
         var target = evt.target;
-        for (var i = 0; i < userImages.length; i++) {
-          if (userImages[i] === target) {
+        userImages.forEach(function (userImage) {
+          if (userImage === target) {
             window.bigPicture.renderBigPicture(data, window.utils.convertStringToNumber(target.getAttribute('src')) - 1);
             bigPictureOpenHandler();
             closeButtonBigPicture.addEventListener('click', bigPictureCloseHandler);
           }
-        }
+        });
       });
     },
   };
